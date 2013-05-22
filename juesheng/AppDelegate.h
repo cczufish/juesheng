229457f216a27bfe5684638b4f7f46461ddd4feb
zjreset <admin.zj@163.com>
2013-05-22 21:14:38 +0800

    Initial Commit

---
+++ juesheng/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  juesheng
+//
+//  Created by runes on 13-5-22.
+//  Copyright (c) 2013年 heige. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
